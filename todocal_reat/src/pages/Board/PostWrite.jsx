b65eb81db4685ffee66d78f57990a6fcc532ac33
raedered <kjh991216@naver.com>
2025-12-09 11:04:04 +0900

    Fix PostWrite style path

--- todocal_reat/src/pages/Board/PostWrite.jsx
+++ todocal_reat/src/pages/Board/PostWrite.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import axios from "../../api/setupAxios";
-import "../../styles/Board/PostWrite.css";
+import "../../styles/board/PostWrite.css";
 
 const PostWrite = () => {
   const navigate = useNavigate();
